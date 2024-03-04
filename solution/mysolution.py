3037daca68a573d1e06bdc9f00e00f693a58186e
Prateek Aranha <u6872485@anu.edu.au>
2024-03-05 01:00:48 +1100

    Add timestep

--- solution/mysolution.py
+++ solution/mysolution.py
@@ -17,6 +17,7 @@ class MySolution(Solution):
     def reset(self, obs, observation_spaces=None, action_spaces=None, seed=None):
         # Currently, the evaluator will NOT pass in an observation space or action space (they will be set to None)
         super().reset(obs, observation_spaces, action_spaces, seed)
+        self.timestep = 0
 
         global_state = next(iter(obs.values()))["globalstate"]
         graph = ObservationHelper.get_multidigraph(global_state)
@@ -25,8 +26,12 @@ class MySolution(Solution):
         # Create an action helper using our random number generator
         self._action_helper = ActionHelper(self._np_random)
 
+    def increment_timestep(self) -> None:
+        self.timestep += 1
+
     def policies(self, obs, dones, infos):
         # Use the acion helper to generate an action
+        self.increment_timestep()
         return self._action_helper.sample_valid_actions(obs)
 
 class PathMatrix:
