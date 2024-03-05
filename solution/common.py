18d56d18bef5f83f03d310c94d7702f8a68fbd22
Dimi Defillet <dimidefillet@hotmail.com>
2024-03-05 11:37:08 +1100

    Fix common

--- solution/common.py
+++ solution/common.py
@@ -1,20 +1,25 @@
-from typing import Dict, List, NamedTuple, Set, Tuple
+from typing import Dict, List, NamedTuple, Set
+
 
 class CargoEdge(NamedTuple):
     cargo_id: int
-    edge: Tuple[int, int]
-    ep: int # earliest pickup
-    lp: int # latest pickup
+    origin: int
+    destination: int
+    ep: int  # earliest pickup
+    lp: int  # latest pickup
     weight: int
     allowed_plane_types: Set[int]
+
 
 class CargoEdges:
     cargo_edges: List[CargoEdge]
 
+
 class Assignments:
     assignments: Dict[int, CargoEdges]
 
-class CommonData:
+
+class Planning:
     def __init__(self, cargo_edges: CargoEdges, assignments: Assignments) -> None:
         self.cargo_edges = cargo_edges
         self.assignments = assignments
