8f79fe4633bba0e94455b82b790beee549e508f2
Prateek Aranha <u6872485@anu.edu.au>
2024-03-05 15:00:50 +1100

    create GroupedRoutes

--- solution/mysolution.py
+++ solution/mysolution.py
@@ -4,7 +4,11 @@ from airlift.envs.airlift_env import ObservationHelper
 
 import networkx as nx
 
-from typing import Tuple, Dict, List
+from collections import defaultdict
+from typing import Tuple, Dict, List, Set
+
+def get_globalstate(obs: Dict) -> Dict:
+    return next(iter(obs.values()))["globalstate"]
 
 class MySolution(Solution):
     """
@@ -19,9 +23,10 @@ class MySolution(Solution):
         super().reset(obs, observation_spaces, action_spaces, seed)
         self.timestep = 0
 
-        global_state = next(iter(obs.values()))["globalstate"]
+        global_state = get_globalstate(obs)
         graph = ObservationHelper.get_multidigraph(global_state)
         self.path_matrix = PathMatrix(graph)
+        self.grouped_routes = GroupedRoutes(obs)
 
         # Create an action helper using our random number generator
         self._action_helper = ActionHelper(self._np_random)
@@ -33,6 +38,37 @@ class MySolution(Solution):
         # Use the acion helper to generate an action
         self.increment_timestep()
         return self._action_helper.sample_valid_actions(obs)
+
+
+class GroupedRoutes:
+    def __init__(self, obs: Dict) -> None:
+        global_state = get_globalstate(obs)
+        self.groups = {
+            plane_type: self._get_subgroup_dict(route)
+            for plane_type, route in global_state["route_map"].items()
+        }
+        self.aircrafts = self._get_aircrafts_in_groups(obs)
+
+    def _get_aircrafts_in_groups(self, obs: Dict) -> Dict[int, Dict[int, Set[str]]]:
+        aircrafts_in_groups = {
+            plane_type: {
+                subgroup: set() for subgroup in subgroup_dict} 
+                for plane_type, subgroup_dict in self.groups.items()
+            }
+        for aircraft, data in obs.items():
+            plane_type = data["plane_type"]
+            for group_key, group_nodes in self.groups[plane_type].items():
+                if data["current_airport"] in group_nodes:
+                    aircrafts_in_groups[plane_type][group_key].add(aircraft)
+                    break
+        return aircrafts_in_groups    
+
+    def _get_subgroup_dict(self, route: nx.graph) -> Dict[int, Set[int]]:
+        return {count: group for count, group in enumerate(self._get_subgroups(route))}
+    
+    def _get_subgroups(self, route: nx.graph) -> List[Set[int]]:
+        return [c for c in nx.weakly_connected_components(route) if len(c) > 1]
+    
 
 class PathMatrix:
     def __init__(self, graph: nx.graph) -> None:
